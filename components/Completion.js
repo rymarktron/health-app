c394c5594707c68db47230f5879a7091e57c42cc
rymarktron <87295558+rymarktron@users.noreply.github.com>
2023-04-28 05:19:29 -0400

    add API

--- components/Completion.js
+++ components/Completion.js
@@ -16,6 +16,49 @@ import Questions from './Questions';
 const Completion = () => {
     const { gameState, setGameState, score, setScore, noQuestions, setNoQuestions} = useContext(QuizContext)
     
+    //add Twilio API to send to phone
+    const [loadingTwilio, setLoadingTwilio]= useState(false)
+    const [phone, setPhone] = useState('')
+ 
+    const sendMessage = (e) => {
+
+        e.preventDefault()
+
+        const accountSid = process.env.NEXT_PUBLIC_TWILIO_SID;
+        const authToken = process.env.NEXT_PUBLIC_TWILIO_AUTH_TOKEN;
+        const from = process.env.NEXT_PUBLIC_FROM_PHONE_NUMBER;
+        
+        //let imageT = document.getElementById('imageGenerated')
+        //console.log(imageT.src)
+        const data = new FormData();
+        data.append('To', phone);
+        data.append('From', from);
+        //data.append('Body', 'Here is a copy of your custom ad');
+        data.append('Body', `Here is a list of the parammeters used to generate your ad copy:\n-Gender: ${gender}\n-Ethnicity: ${ethnicity}\n-Hair color: ${hairColor}\n-Age: ${age}\n-Facial Hair: ${facialHair}\n-Glasses: ${glasses}\n-Earrings: ${earrings}\n-Tatoos: ${tattoos}\n-Time of Day: ${timeOfDay}\n-Location: ${locations}\n-Product: ${productType}\n`);
+        //data.append('MediaUrl', 'https://demo.twilio.com/owl.png')
+        setLoadingTwilio(true)
+
+
+        fetch('https://api.twilio.com/2010-04-01/Accounts/' + accountSid + '/Messages.json', {
+            method: 'post',
+            body: data,
+            headers: {
+                'Authorization': 'Basic ' + btoa(accountSid + ':' + authToken),
+            },
+        }).then(response => {
+            if (response.ok) {
+                setLoadingTwilio(false)
+                setLoading(false)
+                setOpenDialog(false)
+            } else {
+                console.log('Error sending text message: ' + response.status);
+            }
+        }).catch(error => {
+            console.log('Error sending text message: ' + error);
+        });
+    };
+
+
     return ( 
         <div className={styles.end}>
             <h2>{"You have completed " + score + " / 7 of your tasks"}</h2>
@@ -27,12 +70,55 @@ const Completion = () => {
             {noQuestions.map(question => <h3 key={Questions[question].task}>{"Be sure to "+Questions[question].task}</h3>)}
 
             {/* <NoQuestionsList noQuestions={noQuestions}/> */}
+            
+
+            <form onSubmit={sendMessage} className="text-white flex flex-col items-center">
+    <input
+        value={phone}
+        type="tel"
+        className="my-2"
+        required
+        onChange={(e) => setPhone(e.target.value)}
+    />
+    <button
+        disabled={loadingTwilio}
+        className="text-center w-24 bg-blue-500 hover:bg-blue-600 disabled:bg-gray-400 disabled:hover:bg-gray-400 rounded-md h-10"
+        type="submit"
+    >
+        {!loadingTwilio ? (
+            'Send to Phone'
+        ) : (
+            <svg
+                className="animate-spin h-5 w-5 text-white mx-auto"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+            >
+                <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                ></circle>
+                <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0012 20c1.735 0 3.34-.564 4.659-1.512l-2.175-3.762A3.993 3.993 0 0112 16c-1.081 0-2.061-.438-2.767-1.144L6 17.29z"
+                ></path>
+            </svg>
+        )}
+    </button>
+</form>
+            
             <button onClick ={() => {
                 setGameState("start")
                 setScore(0)
                 setNoQuestions([])
-                }}>Fill again</button>
+                }}>Fill Again</button>
         </div>
+        
      );
 }
  
